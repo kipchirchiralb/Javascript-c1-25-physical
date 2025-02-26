74c2318218cb52310e3dcf4d20665dab481566cb
Eldohub Academy <academy@eldohub.co.ke>
2025-02-26 11:11:27 +0300

    variables in js

---
+++ assingments.md
@@ -0,0 +1,5 @@
+## Assignments
+
+- Differentiate between a compiler and an interpretor
+- Discus variable naming rules and variable naming conventions in js
+
---
+++ main.js
@@ -0,0 +1,5 @@
+const teachers = ["Albert", "Kevin", "Brigid", "Isaiah", "Nigel"];
+
+for (teacher in teachers) {
+  console.log("Hello " + teachers[teacher] + ". Welcome to our masterclass.");
+}

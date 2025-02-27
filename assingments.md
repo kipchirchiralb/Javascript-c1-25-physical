dbbdbca5ce6c87021ed2abe3541fb70e7491bea1
Eldohub Academy <academy@eldohub.co.ke>
2025-02-27 10:57:15 +0300

    operatos

--- assingments.md
+++ assingments.md
@@ -2,3 +2,5 @@
 
 - Differentiate between a compiler and an interpretor
 - Discus variable naming rules and variable naming conventions in js
+- What is the difference `==`, `===` in js
+- Explore The Truth Table logic
--- operators.js
+++ operators.js
@@ -5,3 +5,25 @@
    - Assignment: `=`, `+=`, `-=`, etc.
    - Practical: Write a program to check if a number is even or odd
  */
+
+   console.log( 5+5 );
+   console.log( 5-5 );
+   console.log( 5*5 );
+   console.log( 5/5 );
+   console.log( 24%5 ); // 0
+let user = "alber" // assignment operator
+console.log( 5==5 ); // true -- equality check operator
+console.log( 5==6 ); // false
+
+// ! not or negation
+
+console.log( 55 !== 55 );
+console.log( 55 >= 35 );
+
+// AND &&
+console.log( 32>5 || 5>10 ) //  T && T -- T ---- The Truth Table
+
+// OR ||
+
+
+console.log( 68%2 == 0 ); // true - even
